33bc06b1718108a61cb41cceae2d751f96ac0d17
Diarmuid1812 <szustak.m.6@gmail.com>
2020-05-10 22:58:48 +0200

    Onset met A

--- onset.py
+++ onset.py
@@ -5,17 +5,35 @@ from scipy.io import loadmat
 osoba1 = loadmat('osoba_1.mat')['osoba_1']
 osoba2 = loadmat('osoba_2.mat')['osoba_4']
 
-sig = osoba1[0, 0, 2, :]
+sig = osoba1[0, 0, 4, :]
 t = np.arange(0, 2, 0.001)
-# plt.plot(t, sig)
-# plt.show()
+
+K = 1.5
+winLen = 60 #int(np.round(np.sqrt(len(sig))))
+winInd = winLen
+onset = None
+
+mn0 = np.mean(abs(sig[0:winLen]))
+
+while winInd < len(sig)-winLen and onset is None:
+    mnI = mn = np.mean(abs(sig[winInd:winInd+winLen]))
+
+    if mnI > K*mn0:
+        onset = winInd
+    winInd += int(winLen/2)
+
+plt.plot(t, sig)
+plt.show()
+'''
 K = 0.6
 mn = np.mean(abs(sig))
 fir = signal.firwin(60, 0.3, window='hamming')
 filtered = signal.lfilter(fir, 0.1, sig)
-plt.plot(t, filtered)
-plt.show()
+# plt.plot(t, filtered)
+# plt.show()
 for pt in range(len(sig)-1):
     if filtered[pt+1] > 0.6:  # *mn (?)
         onset = pt
         break
+'''
+
