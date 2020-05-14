a1855ceb0b59f9af31cccc799613300e42278f68
pawezim <62487399+pawezim@users.noreply.github.com>
2020-05-14 23:38:35 +0200

    Update klasyfikator.py
    to samo co poprzednio, zapisałem definiując funkcje

--- klasyfikator.py
+++ klasyfikator.py
@@ -2,7 +2,7 @@ from scipy import io
 from scipy import signal
 import numpy as np
 import pandas
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 path = 'C:/Users/Pawel/Desktop/Reka projekt/'
 file = 'osoba_1.mat'
 matdata = io.loadmat(path + file)
@@ -19,31 +19,39 @@ data.shape
 
 #################################################
 
+def averaged_stft_matrix( k, p, m, nwf, nwt, draw_stft=0, draw_signal=0 ) :
+    u = data[k,p,m,:]
+    t,f,z = signal.stft( u, nperseg = 2*np.round(np.sqrt(len(u))) )
+    y = np.abs(z)
+    nf = len(f) # ilosc okien czestotliwosciowych przed usrednieniem
+    nt = len(t) # ilosc okien czasowych przed usrednieniem
+    df = int(nf/nwf) # zadana dlugosc okna czestotliwosciowego
+    dt = int(nt/nwt) # zadana dlugosc okna czasowego
+    A = np.zeros( (nwf, nwt) )
+    for i in range(nwf) :
+        for j in range(nwt) :
+            A[i,j] = np.mean( y[i*df:(i+1)*df, j*dt:(j+1)*dt] )
+    if(draw_stft) :
+        plt.pcolormesh(range(nwf), range(nwt), A)
+        plt.show()
+    if(draw_signal) :
+        plt.plot(u)
+    return A
+
+def stft_features( k, p, nwf, nwt ) :
+    F = np.zeros(201) # macierz do ktorej beda zapisywane cechy stft  i klasa ruchu
+    for m in range(8) : # petla po 8 kanalach EMG
+        F[m*25:(m+1)*25] = np.ndarray.flatten( averaged_stft_matrix(k,p,m,nwf,nwt) )
+    F[200] = k # klasa ruchu (wartosc w zakresie od 0 do 10)
+    return F
+
 nwf = 5 # zadana ilosc okien czestotliwosciowych
 nwt = 5 # zadana ilosc okien czasowych 
-
 L = []
 
 for k in range(11) :
     for p in range(200) :
-        F = np.zeros(201) # macierz do ktorej beda zapisywane cechy stft  i klasa ruchu
-        for m in range(8) : # petla po 8 kanalach EMG
-            u = data[k,p,m,:]
-            t,f,z = signal.stft( u, nperseg = 2*np.round(np.sqrt(len(u))) )
-            y = np.abs(z)
-            nf = len(f) # ilosc okien czestotliwosciowych przed usrednieniem
-            nt = len(t) # ilosc okien czasowych przed usrednieniem
-            df = int(nf/nwf) # zadana dlugosc okna czestotliwosciowego
-            dt = int(nt/nwt) # zadana dlugosc okna czasowego
-            A = np.zeros( (nwf, nwt) )
-            for i in range(nwf) :
-                for j in range(nwt) :
-                        A[i,j] = np.mean( y[i*df:(i+1)*df, j*dt:(j+1)*dt] )
-            #plt.pcolormesh(range(nwf), range(nwt), A)
-            #plt.show()
-            F[m*25:(m+1)*25] = np.ndarray.flatten(A)
-        F[200] = k # klasa ruchu (wartosc w zakresie od 0 do 10)
-        L.append(F)
+        L.append( stft_features(k,p,nwf,nwt) )
 
 df = pandas.DataFrame(L)
 
